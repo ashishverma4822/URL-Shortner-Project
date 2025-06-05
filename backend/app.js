40c94a2a4377f6c0c12c32e709e61cac807a26d6
Ashish Kumar Verma <vashishk0602@gmail.com>
2025-06-05 14:33:45 +0530

    Basic Backend Setup

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {nanoid} from "nanoid";
+import dotenv from "dotenv";
+import connectDB from "./src/config/monogo.config.js";
+import urlSchema from "./src/models/shorturl.model.js";
+dotenv.config("./.env");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post("/api/create", (req,res) => {
+    const {url} = req.body;
+    const shortUrl = nanoid(7);
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl,
+        clicks: 0
+    });
+    newUrl.save();
+})
+
+app.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    const url = await urlSchema.findOne({short_url: id});
+    if (url) {
+        res.redirect(url.full_url);
+    }else{
+        res.status(404).send("URL not found");
+    }
+})
+
+app.listen(3000, ()=>{
+    connectDB();
+    console.log("Server is running on http://localhost:3000");
+})
